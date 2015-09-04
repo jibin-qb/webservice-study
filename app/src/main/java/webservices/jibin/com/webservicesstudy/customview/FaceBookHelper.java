59e49b8d8e92d15401ac3ae705978065b38ce1c2
Jibin S <jibin@Jibins-Mac-Pro.local>
2015-09-04 17:05:16 +0530

    issue fixs

--- app/src/main/java/webservices/jibin/com/webservicesstudy/customview/FaceBookHelper.java
+++ app/src/main/java/webservices/jibin/com/webservicesstudy/customview/FaceBookHelper.java
@@ -3,8 +3,11 @@ package webservices.jibin.com.webservicesstudy.customview;
 import java.util.Arrays;
 import java.util.List;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.facebook.HttpMethod;
 import com.facebook.Request;
+import com.facebook.RequestAsyncTask;
 import com.facebook.Response;
 import com.facebook.Session;
 import com.facebook.SessionState;
@@ -14,12 +17,18 @@ import com.facebook.model.GraphUser;
 public abstract class FaceBookHelper {
 
     private static final String sGRAPH_PATH = "me";
+    private static final String sGRAPH_PATH_posts = "Post";
     public static final List<String> sPERMISSIONS = Arrays.asList(
-            "user_posts ", "publish_actions");
+            "public_profile","user_posts", "publish_actions");
 
     private final Session.StatusCallback mFbStatusCallback = new SessionStatusCallback();
     private MainActivity mPreLoginActivity;
     private Session mFbSession;
+    public static final String DESCRIPTION = "description";
+    public static final String LINK = "link";
+    public static final String PICTURE = "picture";
+    public static final String MESSAGE = "message";
+
 
     public FaceBookHelper(MainActivity context) {
         this.mPreLoginActivity = context;
@@ -60,7 +69,7 @@ public abstract class FaceBookHelper {
         }
         if (mFbSession.isClosed()) {
             fbSessionClearClose();
-            startFaceBookSession();
+            //startFaceBookSession();
         }
     }
 
@@ -72,7 +81,7 @@ public abstract class FaceBookHelper {
             public void onCompleted(GraphUser user, Response response) {
                 mPreLoginActivity.dismissProgressDialog();
                 userDataFetchedFromFaceBook(user);
-                postToFacebookWall(session);
+                postToFacebook(session);
             }
         };
         final Request.GraphUserCallback finalCallback = graphUserCallback;
@@ -89,21 +98,57 @@ public abstract class FaceBookHelper {
         request.executeAsync();
     }
 
+    private void getPostsFromFb(final Session session){
+        final Request.Callback wrapperCallback = new Request.Callback() {
+            @Override
+            public void onCompleted(Response response) {
+
+                //Log.d("response>>>",response.getRawResponse());
+            }
+        };
+        final Bundle params = new Bundle();
+        final Request request = new Request(session, sGRAPH_PATH_posts, params, null,
+                wrapperCallback);
+        request.executeAsync();
+    }
     private class SessionStatusCallback implements Session.StatusCallback {
         @Override
         public void call(Session session, SessionState state,
                          Exception exception) {
             mPreLoginActivity.dismissProgressDialog();
+
+            if (session.getState() == SessionState.CLOSED_LOGIN_FAILED) {
+                fbSessionClearClose();
+            }
             if (session.isOpened()) {
-                mPreLoginActivity.showProgressDialog();
-
-                    return;
-                }
                 fetchUserInfoFromFB(session);
-
+            }
             if (session.getState() == SessionState.CLOSED_LOGIN_FAILED) {
                 fbSessionClearClose();
             }
         }
     }
+    private void postToFacebook(Session mSession) {
+        Request.Callback callback = new Request.Callback() {
+            public void onCompleted(Response response) {
+
+            }
+        };
+        Request request = new Request(mSession, sGRAPH_PATH,
+                createBundleValuesForFb(), HttpMethod.POST, callback);
+        RequestAsyncTask task = new RequestAsyncTask(request);
+        task.execute();
+    }
+    private Bundle createBundleValuesForFb() {
+        Bundle bundle = new Bundle();
+
+
+        bundle.putString(MESSAGE, "cxjkvhxckxchkxcvh");
+
+            bundle.putString(LINK, "cbnkvbkxcvbxcvkb.com");
+           // bundle.putString(NHConstants.PICTURE, mImageUrl);
+
+        return bundle;
+    }
+
 }
--- app/src/main/java/webservices/jibin/com/webservicesstudy/customview/MainActivity.java
+++ app/src/main/java/webservices/jibin/com/webservicesstudy/customview/MainActivity.java
@@ -3,67 +3,116 @@ package webservices.jibin.com.webservicesstudy.customview;
 
 import android.app.ProgressDialog;
 
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 
 
 import com.facebook.Session;
 import com.facebook.model.GraphUser;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import webservices.jibin.com.webservicesstudy.R;
 
 public class MainActivity extends Activity {
 
 
-	ProgressDialog mProgressDialog  ;
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    ProgressDialog mProgressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        try {
+
+            PackageInfo info = getPackageManager().getPackageInfo(
+
+                    "com.gallery.gallerybaraweez",
+
+                    PackageManager.GET_SIGNATURES);
+
+            for (Signature signature : info.signatures) {
+
+                MessageDigest md = MessageDigest.getInstance("SHA");
+
+                md.update(signature.toByteArray());
+
+                Log.d("#####KeyHash:",
+
+                        Base64.encodeToString(md.digest(), Base64.DEFAULT));
+
+            }
+
+        } catch (PackageManager.NameNotFoundException e) {
+
+//
+
+        } catch (NoSuchAlgorithmException e) {
+
+//
+
+        }
+    }
+
+    public void showProgressDialog(String message) {
+
+        try {
+            dismissProgressDialog();
+            if (!(this).isFinishing()) {
+                mProgressDialog = ProgressDialog.show(this, "", message);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+    public void showProgressDialog() {
+        showProgressDialog(getString(R.string.please_wait));
+    }
 
 
-	}
+    public void dismissProgressDialog() {
+        if (!(this).isFinishing()) {
+            if (mProgressDialog != null && mProgressDialog.isShowing()) {
+                mProgressDialog.dismiss();
+            }
+            mProgressDialog = null;
 
-	public void showProgressDialog(String message) {
+        }
+    }
 
-		try {
-			dismissProgressDialog();
-			if (!(this).isFinishing()) {
-				mProgressDialog = ProgressDialog.show(this, "", message);
-			}
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
+    public void postFB(View view) {
+        new FaceBookHelper(this) {
+            @Override
+            public void userDataFetchedFromFaceBook(GraphUser user) {
 
-	}
+            }
 
-	public void showProgressDialog() {
-		showProgressDialog(getString(R.string.please_wait));
-	}
+            @Override
+            public void postToFacebookWall(Session session) {
 
+            }
+        }.startFaceBookSession();
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (data != null) {
+            if (Session.getActiveSession() != null) {
+                Session.getActiveSession().onActivityResult(
+                        MainActivity.this, requestCode, resultCode, data);
+            }
 
-	public void dismissProgressDialog() {
-		if (!(this).isFinishing()) {
-			if (mProgressDialog != null && mProgressDialog.isShowing()) {
-				mProgressDialog.dismiss();
-			}
-			mProgressDialog = null;
-
-		}
-	}
-
-	public void postFB(View view) {
-		new FaceBookHelper(this) {
-			@Override
-			public void userDataFetchedFromFaceBook(GraphUser user) {
-
-			}
-
-			@Override
-			public void postToFacebookWall(Session session) {
-
-			}
-		}.startFaceBookSession();
-	}
+        }
+    }
 }
